b6b8011c8aaf50cf1f903bd102a13d335bcc3505
DilanDrey <dilandr3@estudiantec.cr>
2024-04-30 15:05:55 -0600

    Cambios de la clase
    El commit con todos los cambios realizados en la clase

---
+++ Assets/Scripts/MeetTheKitchenScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MeetTheKitchenScript : MonoBehaviour
+{
+
+    private int isNext = 0;
+    public TextMeshProUGUI text;
+    public TextMeshProUGUI button;
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void nextButton()
+    {
+        switch (isNext)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+    }
+}
--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -2,13 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TestScript : MonoBehaviour
 {
     private AssetBundle myLoadedAssetBundle;
     private string[] scenePaths;
 
+    public TextMeshProUGUI text;
+    public TextMeshProUGUI button;
+    private int isNext = 0;
     private void Start()
     {
         myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/Scenes");
@@ -23,6 +28,20 @@ public class TestScript : MonoBehaviour
         {
             Debug.Log(scenePaths[i]);
         }*/
-        SceneManager.LoadScene("Scene1", LoadSceneMode.Single);
+        switch (isNext)
+        {
+            case 0:
+                text.text = " Primero practicaremos algunos movimientos, para asegurarnos que\ntengas la mejor experiencia durante la actividad";
+                isNext = 1;
+                break;
+            case 1:
+                text.text = "Da click en continuar para iniciar la experiencia.";
+                button.text = "Empezar";
+                isNext = 2;
+                break;
+            case 2:
+                SceneManager.LoadScene("Scene1", LoadSceneMode.Single);
+                break;
+        }
     }
 }
